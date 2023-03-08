bb1a4cee9efeec4ef8ea0b6f98f31886605455e9
clayton-schneider <schneiderc2016@gmail.com>
2023-03-08 08:57:49 -0500

    image changes, added sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,11 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image()],
+  integrations: [
+    tailwind(),
+    react(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
   vite: {
     ssr: {
       noExternal: ["@splidejs/react-splide"],
