047053a6cb7f7e35920b7816722a24538d9b9804
clayton-schneider <schneiderc2016@gmail.com>
2023-04-25 15:14:46 -0400

    added font files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+const FONT_FAMILY_BASE = [
+  "system-ui",
+  "-apple-system",
+  "BlinkMacSystemFont",
+  "Segoe UI",
+  "Roboto",
+  "Oxygen",
+  "Ubuntu",
+  "Cantarell",
+  "Open Sans",
+  "Helvetica Neue",
+  "sans-serif",
+];
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
       xs: "475px",
       ...defaultTheme.screens,
+    },
+    fontFamily: {
+      sans: ["Poppins", ...FONT_FAMILY_BASE],
+      headline: ["Lato", ...FONT_FAMILY_BASE],
     },
     extend: {
       maxWidth: {
