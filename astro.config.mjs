9583bc862724aa8480a9891e628955473309b2f2
clayton-schneider <schneiderc2016@gmail.com>
2023-05-02 19:40:29 -0400

    sitemap and robot added

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,16 +10,23 @@ import react from "@astrojs/react";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   // Needed to remove the backslash in CF Pages
   // https://docs.astro.build/en/reference/configuration-reference/#build-options
   build: {
     format: "file",
   },
+  site: "https://seidelgroup.com",
   integrations: [
     tailwind(),
     react(),
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
   ],
   vite: {
     ssr: {
