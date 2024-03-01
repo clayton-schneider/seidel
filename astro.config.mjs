aa6ca9b1f9b8b8c8d1638efd9c96a5b990bbba7b
clayton-schneider <schneiderc2016@gmail.com>
2024-03-01 15:55:14 -0500

    partytown and plausible

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,24 +13,22 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   // Needed to remove the backslash in CF Pages
   // https://docs.astro.build/en/reference/configuration-reference/#build-options
   build: {
-    format: "file",
+    format: "file"
   },
   site: "https://seidelgroup.com",
-  integrations: [
-    tailwind(),
-    react(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind(), react(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), partytown()],
   vite: {
     ssr: {
-      noExternal: ["@splidejs/react-splide"],
-    },
-  },
+      noExternal: ["@splidejs/react-splide"]
+    }
+  }
 });
